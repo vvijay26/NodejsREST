614cef11e25a0bd25cd6351b5ad142af85f623ec
Vijay Vaidyanathan <vvijay26@gmail.com>
2018-01-11 10:05:32 -0800

    Add files via upload

---
+++ server.js
@@ -0,0 +1,28 @@
+var express = require('express'),
+  app = express(),
+  port = process.env.PORT || 3001,
+  mongoose = require('mongoose'),
+  Task = require('./api/models/todoListModel'), //created model loading here
+  bodyParser = require('body-parser');
+  
+// mongoose instance connection url connection
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/test'); 
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
+var routes = require('./api/routes/todoListRoutes'); //importing route
+routes(app); //register the route
+
+app.use(function(req, res) {
+  res.status(404).send({url: req.originalUrl + ' not found'})
+}); // if no route found for a wrong url etc. send this messages
+
+
+app.listen(port);
+
+
+console.log('todo list RESTful API server started on: ' + port);
